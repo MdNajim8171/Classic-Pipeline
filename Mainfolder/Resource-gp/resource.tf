c00cdcd05a3850a69f042527ee02e0e5d5fb34bf
MdNajim8171 <54273539+MdNajim8171@users.noreply.github.com>
2025-07-26 16:16:06 +0530

    Update resource.tf

--- Mainfolder/Resource-gp/resource.tf
+++ Mainfolder/Resource-gp/resource.tf
@@ -1,7 +1,7 @@
 variable "rg-name" {
     description = "The name of the resource group"
     type        = string
-    default     = "RG_Pipeline-najim"
+    default     = "RG_classic-pipeline"
 }
 
 resource "azurerm_resource_group" "rg121" {
